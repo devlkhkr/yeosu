5c03d81ab658520c8563d61ba0e44ba0cb7aaa9e
LeeKangHyoung <84236655+devlkhkr@users.noreply.github.com>
2024-02-14 17:06:15 +0900

    feat: set config loading

--- quasar.config.js
+++ quasar.config.js
@@ -86,7 +86,9 @@ module.exports = configure(function (ctx) {
 
     // https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js#Property%3A-framework
     framework: {
-      config: {},
+      config: {
+        loading: {},
+      },
 
       // iconSet: 'material-icons', // Quasar icon set
       // lang: 'en-US', // Quasar language pack
@@ -99,7 +101,7 @@ module.exports = configure(function (ctx) {
       // directives: [],
 
       // Quasar plugins
-      plugins: [],
+      plugins: ['Loading'],
     },
 
     // animations: 'all', // --- includes all animations
